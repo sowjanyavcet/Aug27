e01115309a45756323536ac1997ed1f433c1fb41
sowjanya <42107234+sowjanyavcet@users.noreply.github.com>
2019-08-27 22:34:47 -0500

    Add files via upload

---
+++ Aug27.py
@@ -0,0 +1,59 @@
+name="John"
+age=23
+if name=="John" and age==23:
+    print("Your name is John,and you are also 23 years old")
+if name=="John" or name=="Rick":
+    print("our name is either John or Rick")
+    
+    
+    
+ 
+x=2
+if x==2:
+    print("x equals two")
+else:
+    print("x does not equal to two")
+    
+    
+#condition   
+x=2
+print(x==2)
+print(x==3)
+print(x<3)
+
+
+#in operator
+name="John"
+if name in["John","Rick"]:
+    print("your name either John or Rick.")
+    
+    
+#is operator
+x=[1,2,3]
+y=[1,2,3]
+print(x==y)
+print(x is y)
+
+
+astring="Hello world!"
+print(astring[::-1])
+
+
+astring="sowjanya"
+print(astring[::-1])
+
+
+astring="Hello World"
+print(astring.upper())
+print(astring.lower())
+
+astring="Hello World"
+print(astring.startswith("Hello"))
+print(astring.startswith("asdfghjk"))
+
+
+
+
+    
+    
+    
